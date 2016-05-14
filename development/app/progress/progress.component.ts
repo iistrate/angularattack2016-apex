d131f9eff3d7f6c643baa02caf14145142c5ff25
Ionut <ioan@htmlthis.com>
2016-05-14 16:46:01 -0400

    form linking and navigating to active

--- development/app/progress/progress.component.ts
+++ development/app/progress/progress.component.ts
@@ -1,16 +1,28 @@
 import {Component, OnInit} from "@angular/core";
-import { OnActivate, RouteSegment } from "@angular/router";
+import { OnActivate, RouteSegment, Router } from "@angular/router";
+import {Exercise} from "../shared/exercise";
+import {ExerciseService} from "../shared/exercise.service";
 @Component({
-    templateUrl: 'app/progress/progress.tpl.html'
+    templateUrl: 'app/progress/progress.tpl.html',
+    providers: [ExerciseService]
 })
 
 export class ProgressComponent implements OnInit {
     bodyPart: string;
 
-    constructor(private routeSegment: RouteSegment) {}
+    constructor(private routeSegment: RouteSegment, private router: Router, private exerciseService: ExerciseService) {}
 
     ngOnInit():any {
         this.bodyPart = this.routeSegment.getParam('bodyPart');
     }
 
+    onSubmit(f) {
+        this.exerciseService.addToQueue(new Exercise(
+            f.controls['description'].value,
+            +f.controls['sets'].value,
+            +f.controls['reps'].value,
+            f.controls['weight'].value
+        ));
+        this.router.navigate(['/active']);
+    }
 }
