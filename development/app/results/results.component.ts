89dd77b645bfc318aad8ab4e273cfd34ceddf0bd
Ionut <ioan@htmlthis.com>
2016-05-15 15:44:12 -0400

    color changes

--- development/app/results/results.component.ts
+++ development/app/results/results.component.ts
@@ -12,7 +12,7 @@ export class ResultsComponent implements OnInit {
 
     constructor(private pieChartService: PieChartService, private router: Router) {
         this.pieChartService.createPieChart(300, 300,
-            ['#e74c3c', '#2980b9', '#3498db', '#2c3e50', '#000000', '#ffffff', '#FFCF15', '#573D7D', '#FF5F91', '#532C00'],
+            ['#e74c3c', '#2980b9', '#3498db', '#2c3e50', '#000000', '#00B878', '#FFCF15', '#573D7D', '#FF5F91', '#532C00'],
             statList[statList.length-1].sets, '#pieChart');
     }
 
