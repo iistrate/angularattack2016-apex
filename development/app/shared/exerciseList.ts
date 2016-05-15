1f650675d4e99cf48af86bcd5e6d22eedfdb1fdd
Ionut <ioan@htmlthis.com>
2016-05-15 13:00:20 -0400

    removed mock data for exercises

--- development/app/shared/exerciseList.ts
+++ development/app/shared/exerciseList.ts
@@ -1,10 +1,3 @@
 import {Exercise} from "./exercise";
 
-export let exerciseList: Array<Exercise> = [
-    new Exercise('Bench press with dumbbells', 5, 10, 100),
-    new Exercise('Bench press with dumbbells', 5, 10, 100),
-    new Exercise('Bench press with dumbbells', 5, 10, 100),
-    new Exercise('Bench press with dumbbells', 5, 10, 100),
-    new Exercise('Bench press with dumbbells', 5, 10, 100),
-    new Exercise('Bench press with dumbbells', 5, 10, 100),
-];
+export let exerciseList: Array<Exercise> = [];
