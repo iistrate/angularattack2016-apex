5c35b7479579ef8a2abea369e477ca01bbab8da4
Ionut <ioan@htmlthis.com>
2016-05-14 20:06:00 -0400

    timer and timer service

---
+++ development/app/shared/timer.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from "@angular/core";
+import {Timer} from "./timer";
+
+@Injectable()
+
+export class TimerService {
+
+
+    getTimer(timer: Timer):string {
+        return timer.formatted;
+    }
+
+    updateTimer(timer: Timer):void {
+        timer.seconds++;
+        if (timer.seconds % 60 === 0) {
+            timer.seconds = 0;
+            timer.minutes++;
+            if (timer.minutes % 60 === 0) {
+                timer.minutes = 0;
+                timer.hours++;
+                if (timer.hours % 24 === 0) {
+                    timer.hours = 0;
+                }
+            }
+        }
+        let fSeconds = timer.seconds > 10 ? ''+timer.seconds : '0'+timer.seconds;
+        let fMinutes = timer.minutes > 10 ? ''+timer.minutes : '0'+timer.minutes;
+        let fHours = timer.hours > 10 ? ''+timer.hours : '0'+timer.hours;
+
+        timer.formatted = fHours + ':' + fMinutes + ':' + fSeconds;
+
+    }
+}
---
+++ development/app/shared/timer.ts
@@ -0,0 +1,6 @@
+export class Timer {
+    formatted = '00:00:00';
+    seconds: number = 0;
+    minutes: number = 0;
+    hours: number = 0;
+}
