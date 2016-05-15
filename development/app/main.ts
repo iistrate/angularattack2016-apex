c68c64f81b4ac5940a355e9b60fe6482bccb8a31
Ionut <ioan@htmlthis.com>
2016-05-15 19:09:16 -0400

    enable production mode

--- development/app/main.ts
+++ development/app/main.ts
@@ -1,9 +1,10 @@
 /// <reference path="../../typings/browser.d.ts" />
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {ROUTER_PROVIDERS} from '@angular/router';
+import {enableProdMode} from '@angular/core';
 
 import {AppComponent} from './app.component';
-
+enableProdMode();
 bootstrap(AppComponent, [
     ROUTER_PROVIDERS
 ]);
